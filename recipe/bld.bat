332ec9d13d8ba1b1f19e257da9b39601d977b255
Andrii Osipov <aosipov@anaconda.com>
2026-08-25 01:52:44 -0700

    fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,10 @@
 mkdir build
 cd build
 
+:: CMake ≥4.0 removed support for cmake_minimum_required versions below 3.5,
+:: so this CUnit CMakeLists.txt fails to configure with modern CMake.
+set "CMAKE_POLICY_VERSION_MINIMUM=3.5"
+
 cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
